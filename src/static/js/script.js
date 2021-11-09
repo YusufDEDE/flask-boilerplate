9e3cf002a48fbb457df9022f74715fbf7db59356
Yusuf Dede <yusufdede95@hotmail.com>
2021-11-09 17:36:50 +0300

    update script.js

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,7 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("footer-desc").title = "Simple JS DOM Manipulation!"
-}, false);
-
 document.onreadystatechange = function () {
   document.getElementById("footer-desc").innerHTML = "Simple JS DOM Manipulation!"
 }
